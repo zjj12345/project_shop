59d642e0fe0cf834e368eb44fea3b31eda4a16ed
zhang <2776795571@qq.com>
2019-09-15 13:41:49 +0800

    完成基本布局

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,28 +1,52 @@
 <template>
-    <div>
-        <div class="search">
-            <div class="searchbar">
-                <icon type="search"></icon> <span>搜索</span> 
-            </div>
-        </div>
+  <div>
+    <!-- 搜索条 -->
+    <div class="search">
+      <div class="searchbar">
+        <icon type="search"></icon>
+        <span>搜索</span>
+      </div>
     </div>
+    <!-- 轮播图 -->
+    <swiper
+    :indicator-dots="imageFlag"
+    :autoplay="imageFlag"
+    >
+        <swiper-item v-for="(item,index) in imgUrls" :key="index">
+          <image :src="item" class="slide-image" />
+        </swiper-item>
+    </swiper>
+  </div>
 </template>
 
 <script>
 export default {
-}
+    data() {
+        return {
+            imgUrls:[
+                'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+                'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+                'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+            ],
+            imageFlag: true
+        }
+    },
+};
 </script>
 
 <style scoped>
 .search {
-    background-color: #eb4450;
-    padding: 10px;
+  background-color: #eb4450;
+  padding: 10px;
 }
 .searchbar {
-    text-align: center;
-    background-color: #fff;
+  text-align: center;
+  background-color: #fff;
 }
 .search .searchbar icon {
-    vertical-align: middle;
+  vertical-align: middle;
+}
+.slide-image {
+    width: 100%;
 }
 </style>
